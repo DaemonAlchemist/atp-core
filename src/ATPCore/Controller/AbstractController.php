1cc3688f9ca8b671b3db010e31b77f3c5d4ffcd6
Andrew Wittrock <andy@wittrock.us>
2013-11-15 23:02:01 -0600

    Add config retrieval convenience function.

--- src/ATPCore/Controller/AbstractController.php
+++ src/ATPCore/Controller/AbstractController.php
@@ -4,8 +4,23 @@ namespace ATPCore\Controller;
 
 class AbstractController extends \Zend\Mvc\Controller\AbstractActionController
 {
-	public function get($resource)
+	protected function get($resource)
 	{
 		return $this->getServiceLocator()->get($resource);
+	}
+	
+	protected function config($path = null)
+	{
+		$parts = explode(".", $path);
+		$config = $this->get('Config');
+		if(empty($path)) return $config;
+		
+		foreach($parts as $part)
+		{
+			if(!isset($config[$part])) return null;
+			$config = $config[$part];
+		}
+		
+		return $config;
 	}
 }
