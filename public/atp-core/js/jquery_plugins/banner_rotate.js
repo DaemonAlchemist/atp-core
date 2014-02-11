af25c1bf5a590b17bc0f4aef1f30d9993fc8f497
Andrew Wittrock <andy@wittrock.us>
2014-02-11 11:17:36 -0600

    Add buttons to banner rotator

--- public/atp-core/js/jquery_plugins/banner_rotate.js
+++ public/atp-core/js/jquery_plugins/banner_rotate.js
@@ -23,27 +23,75 @@
 			var options = this.options;
 			
 			//Hide banners on first load
-			elem.children().hide();
-			elem.children(':first-child').addClass('banner-displayed');
+			elem.children().addClass("banner").hide();
+			elem.children(':first-child').addClass("banner-displayed");
 			elem.children(':first-child').show();
 			
-			//Rotate banners
-			setInterval(function() {
-				var curBanner =  elem.children('.banner-displayed');
-				var nextBanner = curBanner.next();
-				if(nextBanner.length == 0) nextBanner = elem.children(':first-child');
+			//Add buttons
+			var bannerCount = elem.children().length;
+			elem.append("<div class=\"banner-buttons\"></div>");
+			var buttons = elem.find(".banner-buttons");
+			for(var i=1; i<=bannerCount; i++) {
+				buttons.append("<a href=\"#\" class=\"banner-button\">" + i + "</a>");
+			}
+			buttons.children(":first-child").addClass("active");
+			
+			var rotatorFunc = function() {
+				//Disable buttons while transition is happening
+				buttons.find("a").addClass("disabled");
+			
+				//Get current banner and button
+				var curBanner =  elem.find(".banner-displayed");
+				var curButton = elem.find(".banner-buttons a.active");
 				
-				curBanner.removeClass('banner-displayed');
+				//Update banner index
+				curBannerIndex++;
+				
+				//Get next banner
+				if(curBannerIndex > bannerCount) curBannerIndex = 1;
+				var nextBanner = elem.find(".banner:nth-child("+curBannerIndex+")");
+				var nextButton = elem.find(".banner-buttons a:nth-child("+curBannerIndex+")");
+				
+				//Fade transition between banners
+				curBanner.removeClass("banner-displayed");
 				curBanner.fadeOut({
 					duration: options.bannerFadeTime,
 					complete: function(){
-						nextBanner.addClass('banner-displayed');
 						nextBanner.fadeIn({
-							duration: options.bannerFadeTime
+							duration: options.bannerFadeTime,
+							complete: function(){
+								nextBanner.addClass("banner-displayed");
+								buttons.find("a").removeClass("disabled");
+							}
 						});
 					}
 				});
-			}, options.bannerDisplayTime);
+				
+				//Update button styles
+				curButton.removeClass("active");
+				nextButton.addClass("active");
+			};
+			
+			var curBannerIndex = 1;
+			var rotator = null;
+			buttons.find("a").click(function(evt){
+				//Don't do anything if the link is disabled
+				if($(this).hasClass("disabled")) return false;
+			
+				//Get index of selected banner
+				var index = parseInt($(this).html());
+				curBannerIndex = index - 1;
+				if(curBannerIndex == 0) curBannerIndex = bannerCount;
+				
+				//Show new banner and reset rotator
+				clearInterval(rotator);
+				rotatorFunc();
+				rotator = setInterval(rotatorFunc, options.bannerDisplayTime);
+				return false;
+			});
+			
+			//Rotate banners
+			rotator = setInterval(rotatorFunc, options.bannerDisplayTime);
         },
     };
 
