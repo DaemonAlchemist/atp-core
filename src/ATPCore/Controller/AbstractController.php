ccdceaeea01693d1cee9784bf52290d412558892
Andrew Wittrock <andy@wittrock.us>
2014-09-16 00:09:30 -0500

    Refactor adstract controller and add layout function

--- src/ATPCore/Controller/AbstractController.php
+++ src/ATPCore/Controller/AbstractController.php
@@ -7,42 +7,18 @@ class AbstractController extends \Zend\Mvc\Controller\AbstractActionController
 	protected $_isInstallerController = false;
 	protected $_isUpdaterController = false;
 
-     public function onDispatch(\Zend\Mvc\MvcEvent $event)
-	 {
+	public function onDispatch(\Zend\Mvc\MvcEvent $event)
+	{
 		//Set the default time zone
 		date_default_timezone_set($this->config('timeZone'));
-	 
-		//Check if all modules are up-to-date
-		$modules = $this->config('modules');
-		foreach($modules as $name => $moduleData)
-		{
-			try {
-				$module = new \ATPCore\Model\Module();
-				$module->loadByName($name);
-				
-				if($moduleData['version'] != $module->version)
-				{
-					//Temporarilly disable updater
-					//if(!$this->_isUpdaterController) $this->redirect()->toRoute('install', array('action' => 'update'));
-				}
-			} catch(\Exception $e) {
-				if(!$this->_isInstallerController) $this->redirect()->toRoute('install', array('action' => 'options'));
-			}
-		}
-	 
-		//Detect redirects
-		if(!$this->_isInstallerController && !$this->_isUpdaterController)
-		{
-			if($this->config('redirects.useRedirects'))
-			{
-				$uri = $event->getRequest()->getUri();
-				\ATPCore\Model\Redirect::redirect($uri->getPath(), $uri->getQuery());
-			}
-		}
+
+		$this->_checkForUpdates();
+		$this->_checkForRedirects($event);
+		$this->_initLayout();
 
 		//Proceed with page loading
 		parent::onDispatch($event);
-	 }
+	}
 
 
 	protected function get($resource)
@@ -70,5 +46,63 @@ class AbstractController extends \Zend\Mvc\Controller\AbstractActionController
 		$param = new \ATPCore\Model\Parameter();
 		$param->loadByIdentifier($name);
 		return $param->value;
+	}
+	
+	private function _checkForUpdates()
+	{
+		//Check if all modules are up-to-date
+		$modules = $this->config('modules');
+		foreach($modules as $name => $moduleData)
+		{
+			try {
+				$module = new \ATPCore\Model\Module();
+				$module->loadByName($name);
+				
+				if($moduleData['version'] != $module->version)
+				{
+					//Temporarilly disable updater
+					//if(!$this->_isUpdaterController) $this->redirect()->toRoute('install', array('action' => 'update'));
+				}
+			} catch(\Exception $e) {
+				if(!$this->_isInstallerController) $this->redirect()->toRoute('install', array('action' => 'options'));
+			}
+		}
+	}
+	
+	private function _checkForRedirects($event)
+	{
+		if(!$this->_isInstallerController && !$this->_isUpdaterController)
+		{
+			if($this->config('redirects.useRedirects'))
+			{
+				$uri = $event->getRequest()->getUri();
+				\ATPCore\Model\Redirect::redirect($uri->getPath(), $uri->getQuery());
+			}
+		}
+	}
+
+	private function _initLayout()
+	{
+		$layout = $this->config('layout');
+		
+		//Init blocks
+		foreach($layout['global']['blocks'] as $blockName => $blockData)
+		{
+			$class = $layout['blocks'][$blockName];
+			$block = new $class();
+			$options = array();
+			foreach($blockData['options'] as $optionName => $optionValueData)
+			{
+				$type = $optionValueData[0];
+				$id = $optionValueData[1];
+				switch($type)
+				{
+					case 'siteParam':	$options[$optionName] = $this->siteParam($id);	break;
+					default:			$options[$optionName] = $id;					break;
+				}
+			}
+			$block->setOptions($options);
+			$this->layout()->addChild($block, $blockName);
+		}
 	}
 }
