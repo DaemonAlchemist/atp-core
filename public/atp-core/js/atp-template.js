2fe4886c002660ca01e0330c0d1b9a7aae94c2a1
Andrew Wittrock <andy@wittrock.us>
2015-04-09 10:14:34 -0500

    Update ATP template functions

--- public/atp-core/js/atp-template.js
+++ public/atp-core/js/atp-template.js
@@ -1,17 +1,9 @@
 var ATP = ATP || {};
 
-ATP.partial = function(options) {
-	Mustache.compilePartial(options.name, $(options.selector).html());
+ATP.partial = function(selector) {
+	return $(selector).html();
 };
 
 ATP.template = function(options) {
-	if(typeof ATP.template.templates == 'undefined') {
-		ATP.template.templates = {};
-	}
-	
-	if(typeof ATP.template.templates[options.selector] == 'undefined') {
-		ATP.template.templates[options.selector] = Mustache.compile($(options.selector).html());
-	}
-
-	return ATP.template.templates[options.selector](options.substitutions, options.partials);
+	return Mustache.render($("#" + options.selector).html(), options.view, options.partials);
 };
